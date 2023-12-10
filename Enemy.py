2e98d5adee39c2b1864fbc15a3d5f91cc67bc127
Anonymous <141331552+olenapolianska@users.noreply.github.com>
2023-12-10 18:26:23 +0200

    less2

--- Enemy.py
+++ Enemy.py
@@ -17,4 +17,10 @@ class Enemy:
         window.blit(self.texture, (self.hit_box.x, self.hit_box.y))
 
     def move(self):
-        pass
+        self.hit_box.x += self.speed
+        self.hit_box.y += self.speed
+        if self.hit_box.x > self.x2 and self.hit_box.y > self.y2:
+            self.speed *= -1
+        if self.hit_box.x < self.x1 and self.hit_box.y < self.y1:
+            self.speed *= -1
+
--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import pygame
 import Character
 import Enemy
 import Treasure
+from wall import Wall
+pygame.mixer.init()
+pygame.mixer.music.load("jungles.ogg")
+pygame.mixer.music.play(-1)
+
 
 pygame.init()
 
@@ -11,10 +16,21 @@ fps = pygame.time.Clock()
 fon = pygame.image.load("background3.png")
 fon = pygame.transform.scale(fon, (800,500))
 
-pacman = Character.Character(250, 350, 50, 50, 5, "hero.png")
-cyborg = Enemy.Enemy(280, 380, 50, 50, 5, "cyborg.png", 100, 200, 300, 300)
+pacman = Character.Character(105, 80, 50, 50, 5, "hero.png")
+cyborg = Enemy.Enemy(280, 380, 50, 50, 5, "cyborg.png", 100, 100, 400, 400)
 treasure = Treasure.Treasure(100, 200, 50, 50,  "treasure.png")
+
 game = True
+
+walls = []
+walls. append(Wall(195, 60, 160, 5, (255, 255, 0)))
+walls. append(Wall(295, 450, 120, 5, (255, 255, 0)))
+walls. append(Wall(415, 295, 120, 5, (255, 255, 0)))
+walls. append(Wall(415, 295, 5, 105, (255, 255, 0)))
+
+
+
+
 
 while game:
 
@@ -23,6 +39,11 @@ while game:
     window.fill((123, 123, 123))
     window.blit(fon, (0, 0))
     for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            print(x, y)
+
+
         if event.type == pygame.QUIT:
             game = False
             pygame.quit()
@@ -31,6 +52,8 @@ while game:
     cyborg.move()
     cyborg.render(window)
     treasure.render(window)
+    for wall in walls:
+        wall.render(window)
 
     pygame.display.flip()
     fps.tick(60)
